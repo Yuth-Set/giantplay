fc6a85e3fe96f5d7b96832169a28e6905799014b
yuthset <syuth89@gmail.com>
2016-08-01 11:30:21 +0700

    add social share btn

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -7,6 +7,23 @@
         {!! $article->body !!}
       </div>
       <br/>
+      <!-- AddToAny BEGIN -->
+      <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+      <a class="a2a_dd" href="https://www.addtoany.com/share?linkurl=http%3A%2F%2Fgiantplay.herokuapp.com&amp;linkname=GiantPlay"></a>
+      <a class="a2a_button_facebook"></a>
+      <a class="a2a_button_twitter"></a>
+      <a class="a2a_button_google_plus"></a>
+      <a class="a2a_button_pinterest"></a>
+      <a class="a2a_button_linkedin"></a>
+      </div>
+      <script>
+      var a2a_config = a2a_config || {};
+      a2a_config.linkname = "GiantPlay";
+      a2a_config.linkurl = "http://giantplay.herokuapp.com";
+      </script>
+      <script async src="https://static.addtoany.com/menu/page.js"></script>
+      <!-- AddToAny END -->
+      <br/>
       @if(Auth::user() == $article->user)
         <div class="row">
 
